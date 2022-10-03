d4bca750883e9aad4fd9ddae820212960fa10a18
Kartik Subramani <ksubramani@outlook.com>
2022-10-02 21:17:10 -0400

    Initial Commit of files

---
+++ apps/demo-extension/popup.tsx
@@ -0,0 +1,25 @@
+import { useState } from "react"
+import {exportedFunction} from "@kartycodes/components"
+
+function IndexPopup() {
+  const [data, setData] = useState("")
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        padding: 16
+      }}>
+      <h2>
+        <div>{exportedFunction("Here is the Demo")}</div>
+      </h2>
+      <input onChange={(e) => setData(e.target.value)} value={data} />
+      <a href="https://docs.plasmo.com" target="_blank">
+        View Docs
+      </a>
+    </div>
+  )
+}
+
+export default IndexPopup
---
+++ packages/components/index.ts
@@ -0,0 +1,4 @@
+export function exportedFunction(input: string) {
+   return `I got called with ${input}, and now it has changed`;
+}
+  
